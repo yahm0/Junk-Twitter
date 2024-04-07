84a4db30be094d6ea954c35fd1a91ab239f054ea
DathBitBeard <lanbeard105@gmail.com>
2024-04-06 21:22:02 -0400

    condensed the JS code.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express');
+const router = Router();
 
-// Login route
-router.post('/login', (req, res) => {
-    // Authenticate user
-    // Set session data
-    req.session.user = { /* user data */ };
-    res.send('Logged in successfully');
-});
-
-// Logout route
-router.get('/logout', (req, res) => {
-    // Destroy session
-    req.session.destroy();
-    res.send('Logged out successfully');
-});
-
-// Registration route
-router.post('/register', (req, res) => {
-    // Create new user
-    // Save user data
-    res.send('User registered successfully');
-});
+router.post('/login', (req, res) => { req.session.user = {/* user data */}; res.send('Logged in successfully'); });
+router.get('/logout', (req, res) => { req.session.destroy(); res.send('Logged out successfully'); });
+router.post('/register', (req, res) => res.send('User registered successfully'));
 
 module.exports = router;
+
