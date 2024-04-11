c212d7107d89f05cc25bb2bdedb5442b438d929f
yahm0 <stnt190@outlook.com>
2024-04-10 21:05:40 -0400

    Added the export functionality to the index file for the models.

--- models/index.js
+++ models/index.js
@@ -11,5 +11,23 @@ const likeModel = require('./like');
 
 // Initialize models
 const User = userModel(sequelize, Sequelize.DataTypes);
-const tweet = tweetModel(sequelize, Sequelize.DataTypes);
-const like = likeModel(sequelize, Sequelize.DataTypes);
+const Tweet = tweetModel(sequelize, Sequelize.DataTypes);
+const Like = likeModel(sequelize, Sequelize.DataTypes);
+
+// Define associations
+User.hasMany(Tweet, { foreignKey: 'user_id' });
+Tweet.belongsTo(User, { foreignKey: 'user_id' });
+
+Tweet.hasMany(Like, { foreignKey: 'tweet_id' });
+Like.belongsTo(Tweet, { foreignKey: 'tweet_id' });
+
+User.hasMany(Like, { foreignKey: 'user_id' });
+Like.belongsTo(User, { foreignKey: 'user_id' });
+
+// Export models and Sequelize for use in other files
+module.exports = {
+    User,
+    Tweet,
+    Like,
+    sequelize
+  };
