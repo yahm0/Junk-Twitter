3c6183ba9d935729e65d21cb4e88da394868190c
yahm0 <stnt190@outlook.com>
2024-04-10 20:28:00 -0400

    added attributes for the user table in the user model.

--- models/User.js
+++ models/User.js
@@ -2,3 +2,37 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../db/schema.sql'); 
 
 class User extends Model {}
+
+
+User.init({
+    // Model attributes corresponding to table fields
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    }
+}
