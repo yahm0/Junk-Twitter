38800cff64b8df3fa5b1b8f82d1fbdb70f9c5a85
Landon Beard <lanbeard105@gmail.com>
2024-04-11 00:44:05 -0400

    Torre's help.

--- server.js
+++ server.js
@@ -1,49 +1,43 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const homeRoutes = require('./controllers/homeRoutes');
-const helpers = require('./utils/helpers');
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
+
+// Create a new sequelize store using the express-session package
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-app.use(cookieParser());
+// Configure and link a session object with the sequelize store
 const sess = {
-  secret: process.env.SESSION_SECRET,
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
+  secret: 'Super secret secret',
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
   })
 };
+
+// Add express-session and store as Express.js middleware
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.use(routes);
 
-// Routes
-app.use('/', homeRoutes);
-
-// Default route
-app.get('/', (req, res) => res.send(req.session.username ? `Hello, ${req.session.username}!` : 'Welcome!'));
-
-
-// Start server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
