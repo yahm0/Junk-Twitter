f839fa1f068b5dc39989e63547da91bde1569651
yahm0 <stnt190@outlook.com>
2024-04-10 20:55:30 -0400

    Added logic and fixed some attributes in the like model.

---
+++ models/like.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../db/schema.sql');
+const User = require('./User.js'); // importing the User model
+const Tweet = require('./tweet.js'); // importing the Tweet model
+
+class Like extends Model {}
+
+Like.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    tweet_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tweets', 
+        key: 'id'
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users', 
+        key: 'id'
+      }
+    },
+    is_like: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'Like',
+    timestamps: true, // Let Sequelize handle created_at, updated_at
+    underscored: true, // To ensure the field names use snake_case, matching the SQL table
+    tableName: 'likes', 
+  });
+  
+  module.exports = Like;
